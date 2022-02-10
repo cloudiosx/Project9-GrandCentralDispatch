c148250575b264cedf444eaa183e3658eceaa97c
John Kim <cloudiosx@gmail.com>
2022-02-10 15:03:10 -0500

    Call showError in the background thread and have its alert controller be called in the main thread. Reload the tableview in the parse method in the main thread

--- Project7 - Whitehouse Petitions/ViewController.swift
+++ Project7 - Whitehouse Petitions/ViewController.swift
@@ -32,15 +32,16 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                // We're OK to parse!
-                parse(json: data)
-                return
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    // We're OK to parse!
+                    self.parse(json: data)
+                    return
+                }
             }
+            self.showError()
         }
-        
-        showError()
     }
     
     // Objective-C
@@ -72,7 +73,9 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
     }
     
@@ -88,9 +91,11 @@ class ViewController: UITableViewController {
     // Error handling
     
     func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        DispatchQueue.main.async {
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
     }
     
     // Table view methods
